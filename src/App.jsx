af9225666ab615b15530b86c02cd8fbfbb3cc0c5
abdinazarov7799 <parolxato@gmail.com>
2024-02-28 12:47:28 +0500

    update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+const tg = window.Telegram.WebApp;
 function App() {
-    console.log(window.Telegram,'window.Telegram')
   return (
     <>
-        test
+        <p>initData: {tg?.initData}</p>
+        <p>colorScheme: {tg?.colorScheme}</p>
+        <p>headerColor: {tg?.headerColor}</p>
+        <p>viewportHeight: {tg?.viewportHeight}</p>
+        <p>viewportStableHeight: {tg?.viewportStableHeight}</p>
+        <p>platform: {tg?.platform}</p>
     </>
   )
 }
