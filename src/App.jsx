8606c000de67888ca38a7dfd39a618deba0a3436
abdinazarov7799 <parolxato@gmail.com>
2024-02-28 14:11:36 +0500

    update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ function App() {
         <p>viewportHeight: {tg?.viewportHeight}</p>
         <p>viewportStableHeight: {tg?.viewportStableHeight}</p>
         <p>platform: {tg?.platform}</p>
-        <p>initDataUnsafe: {tg?.initDataUnsafe.user}</p>
+        <p>initDataUnsafe: {tg?.initDataUnsafe?.user?.id}</p>
+        <p>initDataUnsafe: {tg?.initDataUnsafe?.user?.username}</p>
+        <p>initDataUnsafe: {tg?.initDataUnsafe?.user?.first_name}</p>
+        <p>initDataUnsafe: {tg?.initDataUnsafe?.user?.last_name}</p>
+        <img src={tg?.initDataUnSafe?.user?.photo_url} alt=""/>
     </>
   )
 }
