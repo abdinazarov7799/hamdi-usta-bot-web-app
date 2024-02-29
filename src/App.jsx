2c13f59fc9ca6d1cdba1f96438228532d291c450
abdinazarov7799 <parolxato@gmail.com>
2024-02-29 10:20:21 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import './App.css'
+import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
+
 function App() {
+    const {onToggleButton, tg} = useTelegram();
+
     useEffect(() => {
         tg.ready();
-    },[])
-    console.log(tg)
-  return (
-    <>
+    }, [])
 
-    </>
-  )
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+
+const Button = (props) => {
+    return (
+        <button {...props} className={'button ' + props.className}/>
+    );
+};
+
+export default Button;
