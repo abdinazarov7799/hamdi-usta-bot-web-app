663550b41df166d3f942a1e81e8aeb87d70c60b9
abdinazarov7799 <parolxato@gmail.com>
2024-03-19 13:01:25 +0500

    .

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,10 +3,11 @@ import config from "../../config.js";
 import {useTelegram} from "../../hooks/useTelegram.jsx";
 import {get} from "lodash";
 
-const {user} = useTelegram()
+const {user,tg} = useTelegram()
 const request = axios.create({
   baseURL: config.API_ROOT,
   params: {user_id: get(user,'id','926834986')},
 });
-console.log(get(user,'id'),'user id')
+console.log(user,'user id')
+console.log(tg.initDataUnsafe,'initDataUnsafe')
 export { request };
